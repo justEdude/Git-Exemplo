2113c481be47e43a0233ca0921df1cfb8470bc06
Danton Pretti <Dantonprettibcs@hotmail.com>
2025-09-15 09:52:31 -0400

    insercao de readMe

--- src/main/java/com/mycompany/gitexemplo/GitExemplo.java
+++ src/main/java/com/mycompany/gitexemplo/GitExemplo.java
@@ -5,6 +5,9 @@ package com.mycompany.gitexemplo;
 
 public class GitExemplo {
 
+    //nunca esqueca de fazer seus git pull antes de comecar a trabalhar em codigo
+    // e seu git commits antes de um git push
+    
     public static void main(String[] args) {
         
         System.out.println("Hello Git!");
